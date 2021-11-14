9ae7df82ad87bbdf898f585d154beaa4832b9101
Quan Nguyen <quan.nguyen199712@gmail.com>
2021-11-14 15:29:52 -0500

    Add moralis provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MoralisProvider } from 'react-moralis';
+import { MoralisDappProvider } from './providers/MoralisDappProvider/MoralisDappProvider';
 
 import App from './App';
 
+const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
+const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+
+const Application = () => {
+  const isServerInfo = !!(APP_ID && SERVER_URL);
+  if (isServerInfo) {
+    return (
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+        <MoralisDappProvider>
+          <App isServerInfo />
+        </MoralisDappProvider>
+      </MoralisProvider>
+    );
+  }
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      Not working
+    </div>
+  );
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <MoralisProvider
-      appId={process.env.REACT_APP_APPLICATION_ID}
-      serverUrl={process.env.REACT_APP_SERVER_URL}
-    >
-      <App />
-    </MoralisProvider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Application />,
+  // </React.StrictMode>,
   document.getElementById('root'),
 );
